b5ef8287c2b7e3baa0ecc466ee516c57965f3bf2
PeterR <piotrrutkowski97@gmail.com>
2020-03-21 02:20:07 +0100

    b

--- sketch.py
+++ sketch.py
@@ -1,4 +1,4 @@
 from Bio.Align.Applications import ClustalwCommandline
-in_file = "test_fasta.fa"
-clustalw_cline = ClustalwCommandline("clustalw2", infile=in_file)
+in_file = "test.fasta"
+clustalw_cline = ClustalwCommandline("clustalw2", infile=in_file, align=1)
 print(clustalw_cline)

e0ee40aed51b1ee88e45025bb87f689f56e65dc6
PeterR <piotrrutkowski97@gmail.com>
2020-03-22 01:03:42 +0100

    wrapping up

--- multiple_sequence_alignment.py
+++ multiple_sequence_alignment.py
@@ -5,28 +5,26 @@ from Bio import SeqIO
 Entrez.email = "piotrrutkowski97@gmail.com"
 
 def import_dna_from_db(genome_db_codes, spike_protein_db_codes):
+    # write given database entries' dna codes to fasta files
+    print('\nImporting following genomes:\n')
 
     genome_dna = []
     for i in range(len(genome_db_codes)):
         genome_handle = Entrez.efetch(db="nucleotide",id=genome_db_codes[i],rettype="fasta")
         genome = SeqIO.read(genome_handle,"fasta")
         genome_dna.append(genome)
-
         print(genome.description)
-        if i == len(genome_db_codes) - 1:
-            print()
+
+    print('\nImporting following spike proteins:\n')
 
     spike_protein_dna = []
     for i in range(len(spike_protein_db_codes)):
         spike_protein_handle = Entrez.efetch(db="protein",id=spike_protein_db_codes[i],rettype="fasta")
         spike_protein = SeqIO.read(spike_protein_handle,"fasta")
         spike_protein_dna.append(spike_protein)
-
         print(spike_protein.description)
-        if i == len(spike_protein_db_codes) - 1:
-            print()
 
-    # save dna codes imported from database
+    # write imported dna codes to fasta files
     SeqIO.write(genome_dna, 'genome_dna.fasta', 'fasta')
     SeqIO.write(spike_protein_dna, 'spike_protein_dna.fasta', 'fasta')
 
@@ -37,13 +35,9 @@ spike_protein_db_codes = [1812779093, 292660137, 1179780473, 1386872252, 1820506
 
 import_dna_from_db(genome_db_codes, spike_protein_db_codes)
 
-
-
 # multiple sequence alignment using clustalo
 input_files = ['genome_dna.fasta', 'spike_protein_dna.fasta']
 output_files = ['genome_multiple_seq_alignment.fasta', 'spike_protein_multiple_seq_alignment.fasta']
 
 for i in range(len(input_files)):
     os.system('clustalo -i ' + input_files[i] + ' -o ' + output_files[i] + ' --force')
-
-
